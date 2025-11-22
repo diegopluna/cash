d78b3ffa32ab6f807e8abef70bffdc0c7f7a1bb4
Diego Peter <dpeter@dpeter.dev>
2025-11-22 09:45:46 -0300

    feat: add auto-update capabilites

---
+++ src/lib/updater.ts
@@ -0,0 +1,40 @@
+import { invoke } from "@tauri-apps/api/core";
+import { ask, message } from "@tauri-apps/plugin-dialog";
+import { check } from "@tauri-apps/plugin-updater";
+
+export async function checkForAppUpdates(onUserClick: false) {
+  const update = await check();
+  if (update === null) {
+    await message("Failed to check for updates.\n Please try again later.", {
+      title: "Error",
+      kind: "error",
+      buttons: {
+        ok: "OK",
+      },
+    });
+    return;
+  } else if (update !== null) {
+    const yes = await ask(
+      `Update to ${update.version} is available!\n\n Release notes: ${update.body}`,
+      {
+        title: "Update Available",
+        kind: "info",
+        okLabel: "Update",
+        cancelLabel: "Cancel",
+      }
+    );
+
+    if (yes) {
+      await update.downloadAndInstall();
+      await invoke("polite_restart");
+    }
+  } else if (onUserClick) {
+    await message("You are on the latest version!", {
+      title: "No Update Available",
+      kind: "info",
+      buttons: {
+        ok: "OK",
+      },
+    });
+  }
+}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { onMount } from "svelte";  
+  import { checkForAppUpdates } from "../lib/updater";
+  onMount(async () => {
+
+    // Disable right click context menu
+    if (document) {
+      document.addEventListener('contextmenu', e => e.preventDefault())
+    }
+    
+    checkForAppUpdates(false)
+  })
+</script>
+
+<slot />
