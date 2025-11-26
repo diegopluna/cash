3eea20b4a8b34eec34a0daa13b35c61d6c130edf
Diego Peter <dpeter@dpeter.dev>
2025-11-26 20:35:21 -0300

    fix: adjust popover width in faceted filter component
    - Increased the width of the PopoverContent in the data table faceted filter from 200px to 250px for improved usability and better display of content.

--- src/lib/components/data-table/data-table-faceted-filter.svelte
+++ src/lib/components/data-table/data-table-faceted-filter.svelte
@@ -64,7 +64,7 @@
 			</Button>
 		{/snippet}
 	</PopoverTrigger>
-	<PopoverContent class="w-[200px] p-0" align="start">
+	<PopoverContent class="w-[250px] p-0" align="start">
 		<Command>
 			<CommandInput placeholder={title} />
 			<CommandList>
