2550bd1ceae2d172acfa5ac3a779272dabd3829f
Diego Peter <dpeter@dpeter.dev>
2025-11-29 10:45:42 -0300

    feat: add neverthrow for error handling in updater and update dependencies

--- src/lib/updater.ts
+++ src/lib/updater.ts
@@ -1,22 +1,31 @@
 import { invoke } from '@tauri-apps/api/core';
 import { ask, message } from '@tauri-apps/plugin-dialog';
+import { error } from '@tauri-apps/plugin-log';
 import { check } from '@tauri-apps/plugin-updater';
+import { fromPromise } from 'neverthrow';
 
 export async function checkForAppUpdates(onUserClick: boolean = false) {
-	const update = await check();
-	if (update === null) {
-		await message('Failed to check for updates.\n Please try again later.', {
-			title: 'Error',
-			kind: 'error',
-			buttons: {
-				ok: 'OK'
-			}
-		});
+	const result = await fromPromise(check(), (error) => error);
 
+	if (result.isErr()) {
+		if (onUserClick) {
+			await message('Failed to check for updates.\nPlease try again later.', {
+				title: 'Error',
+				kind: 'error',
+				buttons: {
+					ok: 'OK'
+				}
+			});
+		}
+		error(`Failed to check for updates: ${result.error}`);
 		return;
-	} else if (update !== null) {
+	}
+
+	const update = result.value;
+
+	if (update) {
 		const yes = await ask(
-			`Update to ${update.version} is available!\n\n Release notes: ${update.body}`,
+			`Update to ${update.version} is available!\n\nRelease notes: ${update.body}`,
 			{
 				title: 'Update Available',
 				kind: 'info',
